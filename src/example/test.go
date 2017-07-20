7978fb6c05b74bd2f95fb700fa469016af4e42f9
xurui <971661949@qq.com>
2017-07-20 12:14:43 +0800

    添加异常处理器

--- src/example/test.go
+++ src/example/test.go
@@ -3,18 +3,27 @@ package main
 import (
 	"net/http"
 	"rcore"
+	"fmt"
 )
 
 func main(){
 	re := rcore.CreateNewRemux()
 	re.AddMiddleware(testMiddleware)
+	re.AddMiddleware(panicHandler)
+
 	re.SetHandlerMapping("/",http.HandlerFunc(CallbackFun))
+	re.SetHandlerMapping("/panic",http.HandlerFunc(panicTest))
+
 	http.ListenAndServe(":80",re)
 }
 
 
 func CallbackFun(w http.ResponseWriter,req *http.Request){
 	w.Write([]byte("hello world\n"))
+}
+
+func panicTest(w http.ResponseWriter,req *http.Request){
+	panic("err")
 }
 
 
@@ -27,3 +36,20 @@ func testMiddleware(next http.HandlerFunc) http.HandlerFunc{
 	return http.HandlerFunc(f)
 }
 
+func panicHandler(next http.HandlerFunc) http.HandlerFunc{
+
+	f := func(w http.ResponseWriter,req *http.Request){
+
+		defer func(){
+			err := recover()
+			if err!=nil {
+				fmt.Println(err)
+			}
+		}()
+
+		w.Write([]byte("panic forward\n"))
+		next.ServeHTTP(w,req)
+		w.Write([]byte("panic backward\n"))
+	}
+	return http.HandlerFunc(f)
+}
